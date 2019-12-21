bfc0727bbb49dc9a794d3c74cbf137e587c4c72d
Nasruddinkhan <nasruddinkhan44@gmail.com>
2019-12-22 01:26:47 +0530

    hibernate with table per sub class xml based

---
+++ HBProj-InhMapping-TPSC/src/main/java/com/mypractice/test/InheritanceMappingTest.java
@@ -0,0 +1,24 @@
+package com.mypractice.test;
+
+import com.mypractice.dao.InheritanceDAO;
+import com.mypractice.dao.InheritanceDAOFactory;
+import com.mypractice.utility.HibernateUtil;
+
+public class InheritanceMappingTest {
+
+	public static void main(String[] args) {
+		InheritanceDAO dao=null;
+		//get DAO
+		dao=InheritanceDAOFactory.getInstance();
+		System.out.println("-----save data-----");
+		//dao.saveData();
+		
+		System.out.println("-----Display data------");
+		dao.retrieveData();
+		
+		//close Objs
+		HibernateUtil.closeSession();
+		HibernateUtil.closeSessionFactory();
+	}//main
+}//class
+

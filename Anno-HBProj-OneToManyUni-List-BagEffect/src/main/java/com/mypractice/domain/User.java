ec66c8ebf7769eadbc67c656eacc7963ed9de58b
Nasruddinkhan <nasruddinkhan44@gmail.com>
2019-12-25 00:06:19 +0530

    one to many with beg

---
+++ Anno-HBProj-OneToManyUni-List-BagEffect/src/main/java/com/mypractice/domain/User.java
@@ -0,0 +1,67 @@
+package com.mypractice.domain;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.IndexColumn;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+@Entity
+@Table(name = "user_table1")
+public class User {
+	@Id
+	@Column(name = "user_id")
+	private int userId;
+	@Column(name = "first_name")
+	private String firstName;
+	@OneToMany(targetEntity = PhoneNumber.class, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@LazyCollection(value=LazyCollectionOption.EXTRA)
+	@Fetch(FetchMode.SELECT)
+	@JoinColumn(name = "unid", referencedColumnName = "user_id")
+	//@IndexColumn(name="lst_indx",base=1)
+	//@OrderColumn(name="lst_indx")
+	private List<PhoneNumber> phones;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public List<PhoneNumber> getPhones() {
+		return phones;
+	}
+
+	public void setPhones(List<PhoneNumber> phones) {
+		this.phones = phones;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + "]";
+	}
+
+}
